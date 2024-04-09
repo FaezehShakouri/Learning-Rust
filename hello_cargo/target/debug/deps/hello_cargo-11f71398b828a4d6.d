13e3fcd22da10e09737d2185925574e1d8d1b4a4
FaezeShakuri <f.shakori.com@gmail.com>
2024-04-09 18:53:41 +0330

    Delete hello_cargo

--- hello_cargo/target/debug/deps/hello_cargo-11f71398b828a4d6.d
+++
@@ -1,5 +0,0 @@
-/home/akam3/Documents/learning rust/projects/Learning-Rust/hello_cargo/target/debug/deps/libhello_cargo-11f71398b828a4d6.rmeta: src/main.rs
-
-/home/akam3/Documents/learning rust/projects/Learning-Rust/hello_cargo/target/debug/deps/hello_cargo-11f71398b828a4d6.d: src/main.rs
-
-src/main.rs:
